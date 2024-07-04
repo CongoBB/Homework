73207a39e2c0b50f0a2d69ef95bfae6dfbd087fb
CongoBB <36362414+CongoBB@users.noreply.github.com>
2024-07-04 14:05:31 +0300

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,10 @@
+# 02.07.24
+# variables
+# Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.
+
+completed_home_tasks = 12
+hours_spent_in_total = 1.5
+course_name = 'Python'
+average_time_spent_on_a_task = hours_spent_in_total / completed_home_tasks
+print('Курс:', course_name + ', всего задач:', str(completed_home_tasks) + ', затрачено часов:',
+      str(hours_spent_in_total) + ', среднее время выполнения ' + str(average_time_spent_on_a_task), 'часа.')
