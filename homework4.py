f2bb9015570740adf96b1939de7d8679e3e45d0d
CongoBB <36362414+CongoBB@users.noreply.github.com>
2024-07-04 19:55:25 +0300

    Add files via upload
    04.07

---
+++ homework4.py
@@ -0,0 +1,10 @@
+# 04.07
+print('Введите любые ФИО полностью')
+my_string = input()
+print('Количество введенных символов: ', len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string)
+print(my_string[0])
+print(my_string[-1])
