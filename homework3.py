d7ef53668b5c9d4f4019232a9e9701b403eab978
CongoBB <36362414+CongoBB@users.noreply.github.com>
2024-07-04 14:07:30 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,9 @@
+# 3.07
+name = 'Vladimir'
+print('Name:', name)
+age = 29
+print('Current age:', age)
+age = age + 1
+print('New age:', age)
+is_student = True
+print('Vladimir is a student:', is_student)
