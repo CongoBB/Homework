9feaec819d850c3b4632304120ccab6dc183c196
CongoBB <36362414+CongoBB@users.noreply.github.com>
2024-07-11 22:08:19 +0300

    Update module_2_4.py

--- module_2_4.py
+++ module_2_4.py
@@ -3,16 +3,17 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
 
-for i in range(1, len(numbers)):
-    isPrime = True
-    for j in range(2, i):
-        if numbers[i] % j == 0 or numbers[i] / j**0.5 == 0:
-            isPrime = False
-            break
-    if isPrime:
-        primes.append(numbers[i])
-    else:
-        not_primes.append(numbers[i])
+for i in numbers:
+    if i > 1:
+        isPrime = True
+        for j in range(2, i):
+            if i % j == 0 or i / j**0.5 == 0:
+                isPrime = False
+                break
+        if isPrime:
+            primes.append(i)
+        else:
+            not_primes.append(i)
 
 print(primes)
 print(not_primes)
