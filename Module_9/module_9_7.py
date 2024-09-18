9f724cac437cba0d0e042a6bc41325d0f6ba84d0
CongoBB <36362414+CongoBB@users.noreply.github.com>
2024-09-18 17:00:29 +0300

    Add files via upload

---
+++ Module_9/module_9_7.py
@@ -0,0 +1,24 @@
+def is_prime(func):
+    def wrapper(*args):
+        result_wrapper = func(*args)
+        if result_wrapper == 2:
+            return "Простое число"
+        for i in range(2, result_wrapper):
+            if result_wrapper % i == 0 or result_wrapper / i ** 0.5 == 0:
+                return "Составное число"
+        return "Простое число"
+
+    return wrapper
+
+
+@is_prime
+def sum_numbers(*args):
+    summ = 0
+    for i in args:
+        summ += i
+    print(summ)
+    return summ
+
+
+result = sum_numbers(2, 3, 6)
+print(result)
