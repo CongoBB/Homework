e4903d3b4cc31cabfac09ec91f13b37ba78cd240
CongoBB <36362414+CongoBB@users.noreply.github.com>
2024-09-19 20:13:46 +0300

    Update module_9_7.py

--- Module_9/module_9_7.py
+++ Module_9/module_9_7.py
@@ -2,19 +2,20 @@ def is_prime(func):
     def wrapper(*args):
         result_wrapper = func(*args)
         if result_wrapper == 2:
-            return "Простое число"
+            print("Простое число")
+            return result_wrapper
         for i in range(2, result_wrapper):
             if result_wrapper % i == 0 or result_wrapper / i ** 0.5 == 0:
-                return "Составное число"
-        return "Простое число"
-
+                print("Составное число")
+                return result_wrapper
+        print("Простое число")
+        return result_wrapper
     return wrapper
 
 
 @is_prime
 def sum_numbers(*args):
     summ = sum(args)
-    print(summ)
     return summ
 
 
