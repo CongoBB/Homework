fdd3c9baea0cbe7f260a42bcc2924c8b442ea660
CongoBB <36362414+CongoBB@users.noreply.github.com>
2024-11-22 10:57:52 +0300

    22.11
    Выбор элементов и функции в SQL запросах

---
+++ Module_14/module_14_2.py
@@ -0,0 +1,41 @@
+import sqlite3
+# import random
+# from pprint import pprint
+
+
+connection = sqlite3.connect("not_telegram.db")
+# connection.row_factory = sqlite3.Row
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER, 
+balance INTEGER NOT NULL
+)
+''')
+
+cursor.execute(' CREATE INDEX IF NOT EXISTS idx_email ON Users (email)')
+
+# for i in range(1, 11):
+#     cursor.execute(' INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)',
+#                    (f'User{i}', f'example{i}@gmail.com', str(random.randint(58, 62)), str(1000)))
+
+# cursor.execute(' UPDATE Users SET balance = balance - 500 WHERE id = 1')
+cursor.execute(' DELETE FROM Users WHERE id = 6 ')
+
+cursor.execute('SELECT COUNT(*) FROM Users')
+total_users = cursor.fetchone()[0]
+
+cursor.execute('SELECT SUM(balance) FROM Users')
+all_balances = cursor.fetchone()[0]
+print(all_balances/total_users)
+
+# cursor.execute(' SELECT username, email, age, balance FROM Users WHERE age != 60')
+# users = cursor.fetchall()
+# for user in users:
+#     print(f'Имя: {user["username"]} | Почта: {user["email"]} | Возраст: {user["age"]} | Баланс: {user["balance"]}')
+connection.commit()
+connection.close()
